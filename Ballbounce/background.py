09f86f3d96e8174fd55e6815da6ba9a417c8b868
Faunja <129631000+Faunja@users.noreply.github.com>
2025-02-09 18:10:36 -0500

    Updated files via upload

--- Ballbounce/background.py
+++ Ballbounce/background.py
@@ -16,22 +16,23 @@ def draw_text_background(tabbed, shift, space, friction, collision):
 		draw_text("Tab - Hide Instructions", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16)])
 		draw_text("Enter - Create Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 2])
 		draw_text("Backspace - Delete Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 3])
-		draw_text("R key - Reset Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 4])
-		if shift == False:
-			draw_text("Left Click - Grab Ball", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 5])
-			draw_text("Right Click - Sling Ball", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 6])
-		else:
-			draw_text("Left Click - Pull Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 5])
-			draw_text("Right Click - Push Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 6])
-		draw_text("Shift - Change Mouse Mode", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 7])
+		draw_text("R Key - Reset Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 4])
 		if friction != 1:
-			draw_text("F key - Disable Friction", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 8])
+			draw_text("F Key - Disable Friction", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 5])
 		else:
-			draw_text("F key - Enable Friction", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 8])
+			draw_text("F Key - Enable Friction", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 5])
 		if collision == True:
-			draw_text("C key - Disable Ball Collision", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 9])
+			draw_text("C Key - Disable Ball Collision", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 6])
 		else:
-			draw_text("C key - Enable Ball Collision", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 9])
+			draw_text("C Key - Enable Ball Collision", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 6])
+		if shift == False:
+			draw_text("Shift - Enable Mouse Gravity", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 7])
+			draw_text("Left Click - Grab Ball", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 8])
+			draw_text("Right Click - Sling Ball", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 9])
+		else:
+			draw_text("Shift - Disable Mouse Gravity", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 7])
+			draw_text("Left Click - Pull Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 8])
+			draw_text("Right Click - Push Balls", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 9])
 		if space == False:
 			draw_text("Space Bar - Disable Gravity", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 10])
 			draw_text("WASD / Arrow Keys - Change Gravity", [round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 16) * 11])
